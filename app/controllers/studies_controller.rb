39d8851fccd69c6f888a200004d3e3f14efe54f6
Luis Martinez <luis_mmartinez@live.com>
2021-05-02 22:02:04 -0400

    write index html page

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -1,2 +1,13 @@
 class StudiesController < ApplicationController
+
+
+  def index
+    @studies = Study.all
+  end
+
+  def show
+    @study = Study.first
+    redireto_to study_path(@study)
+  end
+
 end
