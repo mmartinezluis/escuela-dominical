2752066aab58d243bd4a8e89505aeb6ed3fb0b5e
Luis Martinez <luis_mmartinez@live.com>
2021-05-03 19:12:01 -0400

    add #subtitles method and #points method to Study model.

--- app/models/study.rb
+++ app/models/study.rb
@@ -1,3 +1,12 @@
 class Study < ApplicationRecord
   serialize :outline, Hash
+
+  def subtitles
+    self.outline[:subtitles].keys
+  end
+
+  def points
+    self.outline[:subtitles].values
+  end
+  
 end
